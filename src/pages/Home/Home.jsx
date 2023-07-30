0575dfbf29453f5dd37ce125948cda990cb184ad
MoinUddin005 <moin368892@gmail.com>
2023-07-30 21:02:04 +0600

    cretiveCard complited done

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,8 +3,8 @@ import React from 'react';
 import Branding from './Branding/Branding';
 import Brands from './Brands/Brands';
 import Services from './Services/Services';
+import OurWorks from './OurWorks/OurWorks';
 // import Faq from './Faq/Faq';
-// import OurWorks from './OurWorks/OurWorks';
 
 const Home = () => {
    return (
@@ -12,8 +12,8 @@ const Home = () => {
          <Branding />
          <Brands />
          <Services />
-         {/* <OurWorks />
-         <Faq /> */}
+         <OurWorks/>
+         {/* <Faq /> */}
         
       </div>
    );
